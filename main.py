572264541cb512eb01cb8cf27581424e6dc5cbeb
İrem Çıngı <iremcingi@protonmail.com>
2020-06-22 22:37:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -64,13 +64,18 @@ if __name__ == "__main__":
     training_set_inputs = array([[0, 0, 1], [1, 1, 1], [1, 0, 1], [0, 1, 1]])
     training_set_outputs = array([[0, 1, 1, 0]]).T
 
-    # Train the neural network using a training set.
-    # Do it 10,000 times and make small adjustments each time.
-    neural_network.train(training_set_inputs, training_set_outputs, 10000)
+    neural_network.train(training_set_inputs, training_set_outputs, 5)
 
-    print "New synaptic weights after training: "
-    print neural_network.synaptic_weights
+    print ("New synaptic weights after training 1: ")
+    print (neural_network.synaptic_weights)
 
-    # Test the neural network with a new situation.
-    print "Considering new situation [1, 0, 0] -> ?: "
-    print neural_network.think(array([1, 0, 0]))
+    print ("Considering new situation 1 [1, 0, 0] -> ?: ")
+    print (neural_network.think(array([1, 0, 0])))
+
+    neural_network.train(training_set_inputs, training_set_outputs, 100)
+
+    print ("New synaptic weights after training 2: ")
+    print (neural_network.synaptic_weights)
+
+    print ("Considering new situation [1, 0, 0] -> ?: ")
+    print (neural_network.think(array([1, 0, 0])))
